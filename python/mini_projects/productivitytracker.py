6e56a54785f2addf2afd601107033ad9f68302e5
hareeshreddy8 <harreeshreddychinnakondu48@outlook.com>
2026-04-14 07:05:58 +0530

    wrote comments

--- python/mini_projects/productivitytracker.py
+++ python/mini_projects/productivitytracker.py
@@ -1,13 +1,24 @@
+# CLI Productivity Tracker(phase 1 - In memory version)
+# Features :
+#  -- Add task
+#   -- display Task
+
+
+#Creates task object with incomplete status
 def create_task(task):
     task_view = {"name":task,"status":False}
     return task_view
 tasks = []
+#ask user how many tasks they want to add
 print("how many tasks do you need to add")
 no_of_tasks = int(input())
+#loop to take multiple task inputs from users
 for _ in range(no_of_tasks):
     task = str(input("Enter task name :"))
     tasks.append(create_task(task))
 
+
+#Display all tasks with index and completion status
 def display_tasks(tasks):
     for index,task in enumerate(tasks):
         if not task["status"]:
