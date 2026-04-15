0d2132bd058b8aa6fadd135555d828c609c9b6d5
hareeshreddy8 <hareeshreddychinnakondu48@outlook.com>
2026-04-15 07:33:04 +0530

    feat:persist tasks using a JSON file (tasks.json)

--- python/mini_projects/productivitytracker.py
+++ python/mini_projects/productivitytracker.py
@@ -4,28 +4,25 @@
 #1.Add task
 #2.Display Task
 #3.Marks tasks as completed 
+#4.
 #--------------------------------------------------------
 
-
+import json
 #Creates task object with default status (not completed)
 def create_task(task):
-    task_view = {"name":task,"status":False}
-    return task_view
-
+    return {"name":task,"status":False}
+    
 
 #Mark task as done using its index
 def mark_task_done(tasks,index):
     if not tasks:
-        print("No tasks available.")
-        return
+        raise ValueError("No tasks available")
     if index >= len(tasks) or index < 0:
-        print("invalid index,Please try again.")
-        return 
+        raise IndexError(f"Invalid index.Please enter between {0} and {len(tasks)-1}")
     #update task with that index  
     tasks[index]["status"] = True
-    print("Task marked as completed.")
-    print("Current tasks:")
-    display_tasks(tasks)
+
+    
 
 #Display all tasks with their index and completion status
 def display_tasks(tasks):
@@ -49,13 +46,30 @@ def index_of_task_completed(tasks):
             index = int(input("enter task number to mark as done:"))
             if index >= len(tasks) or index < 0 :
                 print(f"Invalid index,Please enter between {0} to {len(tasks) - 1}")
+            
             else:
                 return index
-        except:
+        except ValueError:
             print(f"Invalid input.Please enter a number.")
+#load file contents into our memory
+def load_tasks():
+    try :
+        with open("tasks.json","r") as file :
+            tasks = json.load(file)
+            return tasks
+    except FileNotFoundError:
+        return []
+    
 
+#to save the contents in memory to file to not lose them
+def save_tasks(tasks):
+    with open("tasks.json","w") as file :
+        json.dump(tasks,file)
+
+
+           
 #creating menu for users to decide what they want to do
-tasks = []
+tasks = load_tasks()
 while True :
     print("===== MENU =====")
     print("1. Add Task")
@@ -64,27 +78,31 @@ while True :
     print("4. Exit")
     print("================")
     try :
-        choice = int(input("Enter your choice:"))
+        choice = int(input("Enter your choice: "))
 
     except:
         print("Invalid input. Please enter a number.")
         continue
 
     if choice == 1:
-        print("Enter task name:")
+        print("Enter task name: ")
         task = input()
         tasks.append(create_task(task))
-        print("Task created succesfully.")
+        save_tasks(tasks)
+        print("Task created successfully.")
 
     elif choice == 2:
         display_tasks(tasks)
 
     elif choice == 3:
-        if not tasks :
-            print("No tasks available.")
-        else :
+        try:
             index = index_of_task_completed(tasks)
             mark_task_done(tasks,index)
+            save_tasks(tasks)
+            print("Task marked as completed. Updated tasks:")
+            display_tasks(tasks)
+        except (ValueError,IndexError) as e :
+            print(e)
         
 
     elif choice == 4 :
