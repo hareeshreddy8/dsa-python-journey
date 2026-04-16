d93347944ed13e77e491fd73dc0b908d71e1de97
hareeshreddy8 <hareeshreddychinnakondu48@outlook.com>
2026-04-16 06:46:10 +0530

    feat : Delete task,Edit task,__name__ check

--- python/mini_projects/productivitytracker.py
+++ python/mini_projects/productivitytracker.py
@@ -4,10 +4,29 @@
 #1.Add task
 #2.Display Task
 #3.Marks tasks as completed 
-#4.
+#4.File persistent(using JSON)
+#5.Delete task
+#Edit task
 #--------------------------------------------------------
 
 import json
+FILE_NAME = "tasks.json"
+#load file contents into our memory
+def load_tasks():
+    try :
+        with open(FILE_NAME,"r") as file :
+            tasks = json.load(file)
+            return tasks
+    except FileNotFoundError:
+        return []
+    
+
+#to save the contents in memory to file to not lose them
+def save_tasks(tasks):
+    with open(FILE_NAME,"w") as file :
+        json.dump(tasks,file)
+
+
 #Creates task object with default status (not completed)
 def create_task(task):
     return {"name":task,"status":False}
@@ -21,6 +40,25 @@ def mark_task_done(tasks,index):
         raise IndexError(f"Invalid index.Please enter between {0} and {len(tasks)-1}")
     #update task with that index  
     tasks[index]["status"] = True
+
+
+#Delete tasks using its index
+def delete_task(tasks,index):
+    if not tasks :
+        raise ValueError("No tasks available. ")
+    if index >= len(tasks) or index < 0 :
+        raise IndexError(f"Invalid index.Please enter between {0} and {len(tasks)-1}")
+    
+    #update tasks by deleting using its index 
+    tasks.pop(index)
+
+#Edit name of existing task using index
+def edit_task(tasks,index : int,new_name:str):
+    if not tasks:
+        raise ValueError("No tasks available.")
+    if not 0 <= index < len(tasks) :
+        raise IndexError(f"Invalid index.Please enter between {0} and {len(tasks) - 1}")
+    tasks[index]["name"] = new_name
 
     
 
@@ -40,10 +78,10 @@ def display_tasks(tasks):
                   
 
 #Get a valid task index from the user with proper validation
-def index_of_task_completed(tasks):
+def index_of_task(tasks):
     while True:
         try:
-            index = int(input("enter task number to mark as done:"))
+            index = int(input("enter task number: "))
             if index >= len(tasks) or index < 0 :
                 print(f"Invalid index,Please enter between {0} to {len(tasks) - 1}")
             
@@ -51,67 +89,95 @@ def index_of_task_completed(tasks):
                 return index
         except ValueError:
             print(f"Invalid input.Please enter a number.")
-#load file contents into our memory
-def load_tasks():
-    try :
-        with open("tasks.json","r") as file :
-            tasks = json.load(file)
-            return tasks
-    except FileNotFoundError:
-        return []
-    
-
-#to save the contents in memory to file to not lose them
-def save_tasks(tasks):
-    with open("tasks.json","w") as file :
-        json.dump(tasks,file)
 
 
            
 #creating menu for users to decide what they want to do
-tasks = load_tasks()
-while True :
-    print("===== MENU =====")
-    print("1. Add Task")
-    print("2. View Tasks")
-    print("3. Mark Task as Done")
-    print("4. Exit")
-    print("================")
-    try :
-        choice = int(input("Enter your choice: "))
+def main():
+    tasks = load_tasks()
+    while True :
+        print("===== MENU =====")
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Mark Task as Done")
+        print("4. Delete Task")
+        print("5. Edit Task Name")
+        print("6. Exit")
+        print("================")
+        try :
+            choice = int(input("Enter your choice: "))
 
-    except:
-        print("Invalid input. Please enter a number.")
-        continue
+        except:
+            print("Invalid input. Please enter a number.")
+            continue
 
-    if choice == 1:
-        print("Enter task name: ")
-        task = input()
-        tasks.append(create_task(task))
-        save_tasks(tasks)
-        print("Task created successfully.")
-
-    elif choice == 2:
-        display_tasks(tasks)
-
-    elif choice == 3:
-        try:
-            index = index_of_task_completed(tasks)
-            mark_task_done(tasks,index)
+        if choice == 1:
+            print("Enter task name: ")
+            task = input()
+            tasks.append(create_task(task))
             save_tasks(tasks)
-            print("Task marked as completed. Updated tasks:")
+            print("Task created successfully.")
+
+        elif choice == 2:
             display_tasks(tasks)
-        except (ValueError,IndexError) as e :
-            print(e)
-        
 
-    elif choice == 4 :
-        break
+        elif choice == 3:
+            if not tasks :
+                print("NO tasks available")
+                continue
+            try:
+                index = index_of_task(tasks)
+                mark_task_done(tasks,index)
+                save_tasks(tasks)
+                print("Task marked as completed. Updated tasks:")
+                display_tasks(tasks)
+            except (ValueError,IndexError) as e :
+                print(e)
+            
 
-    else :
-        print("Invalid choice. Please try again.")
+        elif choice == 4 :
+            if not tasks :
+                print("NO tasks available")
+                continue
+            try :
+                index = index_of_task(tasks)
+                delete_task(tasks,index)
+                save_tasks(tasks)
+                print("Task deleted successfully. Updated tasks: ")
+                display_tasks(tasks)
 
-        
+            except (ValueError,IndexError) as e:
+                print(e)
+        elif choice == 5:
+            if not tasks:
+                print("No tasks available")
+                continue
+            try :
+                index = index_of_task(tasks)
+                while True :
+                    new_name = str(input("Enter a new name to task: "))
+                    if new_name.strip():
+                        break
+                    else :
+                        print("Please enter valid name")
+                edit_task(tasks,index,new_name)
+                save_tasks(tasks)
+                print("Task name updated successfully. Updated tasks")
+                display_tasks(tasks)
 
+            except ValueError:
+                print("Please enter a valid name.")
+
+        elif choice == 6:
+            break
+
+        else :
+            print("Invalid choice. Please try again.")
+
+
+#using __name__ check 
+
+if __name__ == "__main__" :
+    main()
 
     
